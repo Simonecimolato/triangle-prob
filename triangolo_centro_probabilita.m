70d440b008aca4569bef15ee6e8f13c1cb77b71e
Simone Cimolato <Simonecimolato@users.noreply.github.com>
2022-06-27 21:53:51 +0200

    Update triangolo_centro_probabilita.m

--- triangolo_centro_probabilita.m
+++ triangolo_centro_probabilita.m
@@ -6,7 +6,7 @@ clc
 
 %% DATA
 
-fast = true;
+fast = false;
 
 i = 1;
 n = 10000;
@@ -34,7 +34,7 @@ while i < n
 
         probU = count /i
 
-        plot(x,y)
+        plot(x,y, LineWidth = 2)
     
         hold on
         grid on
@@ -48,7 +48,8 @@ while i < n
         xp = cos(tri);
         yp = sin(tri);
 
-        plot(xp,yp)
+        plot(xp,yp, LineWidth = 2)
+        plot(0,0,'*', LineWidth = 2)
         
         hold off
         pause(0.1);
